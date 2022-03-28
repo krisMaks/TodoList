b5ccd84d1ee24f7e51ab745bae5555e90e3e2897
Maksimova Kristina <kris.maks.t.7@gmail.ru>
2022-03-28 14:23:09 +0300

    Initial Commit

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Кристина Максимова on 28.03.2022.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
